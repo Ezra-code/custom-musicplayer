7debb5d1d753a00d468d318c145ce90b84991f23
Ezra-code <njerukithinji872@gmail.com>
2022-06-24 13:22:48 +0300

    update the buttons on the song cards

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchAllSongs()
+    submitNewMusic()
+    fetchLocalMusic()
 })
 
 
 function fetchAllSongs(){
-    fetch('https://acnhapi.com/v1a/songs/?_limit=20')
+    fetch('https://acnhapi.com/v1a/songs')
     .then(res => res.json())
     .then(songs => {
         songs = songs.slice(0, 10)
@@ -31,11 +33,73 @@ function displaySongs(song){
         </div>
 
         <div class="price-button">
-            <button id="buy">buy</button>
-            <button id="sell">sell</button>
+            <button id="buy">Favorite <i class="fa-solid fa-heart"></i></button>
+            <button id="sell">Like <i class="fa-solid fa-thumbs-up"></i></button>
         </div>
 
-        <button id="edit">Edit</button>
     </div>
     `
 }
+//     let url = 'http://localhost:3000/songs'
+//     let music = {}
+// function uploadNewMusic(){
+//     fetch(url, {
+//         method:'POST',
+//         headers:{
+//             "content-type":"application/json",
+//             Accept:"application/json"
+//         },
+//         body:JSON.stringify(music)
+//     })
+// }
+
+// function submitNewMusic(){
+//     document.querySelector('form').addEventListener('submit', (e) =>{
+//         e.preventDefault()
+//         const sName = document.getElementById('sname').value
+//         const sPrice = document.getElementById('sprice').value
+//         const songLink = document.getElementById("song").value
+//         const songImage = document.getElementById("image").value
+//         console.log(songLink)
+//         document.querySelector('form').reset()
+//         if(sName == "" || sPrice == ""){
+//             document.getElementById('error').innerHTML = "all input fields are required".toUpperCase()
+//         }else{
+//             music.name = sName
+//             music.price = sPrice
+//             music.src = songLink
+//             music.img = songImage
+//         }
+//         uploadNewMusic()
+//     })
+// }
+// function fetchLocalMusic(){
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => data.forEach(song=>{
+//         displayLocalSongs(song)
+//     }))
+// }
+
+// function displayLocalSongs(song){
+//     let displayDiv = document.querySelector(".cards")
+//     displayDiv.innerHTML += `  
+//     <div class="music-card">    
+//         <img src="${song.img}" alt="" id="img">
+
+//         <h3>${song["name"]}</h3>
+
+//         <audio controls>
+//             <source src="${song["src"]}">
+//         </audio>
+
+//         <button id="edit">Edit</button>
+//     </div>
+//     `
+// }
+
+
+// {/* <div id="price">
+// <p id="buy-price">buying price: <span>${song["buy-price"]}</span></p>
+// <p id="sell-price">selling price: <span>${song["sell-price"]}</span></p>
+// </div> */}
