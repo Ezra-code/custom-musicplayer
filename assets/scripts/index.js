51431651038020bc46431b9022cbaca4e2a15b7e
Ezra-code <njerukithinji872@gmail.com>
2022-06-24 05:43:12 +0300

    add content from the api to html document

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,29 +13,29 @@ function fetchAllSongs(){
         })
     })
 }
-// function displaySongs(song){
-//     let displayDiv = document.querySelector(".cards")
-//     displayDiv.innerHTML += `  
-//     <div class="music-card">    
-//         <img src="${song["image_uri"]}" alt="" >
+function displaySongs(song){
+    let displayDiv = document.querySelector(".cards")
+    displayDiv.innerHTML += `  
+    <div class="music-card">    
+        <img src="${song["image_uri"]}" alt="" id="img">
 
-//         <h3>${song["file-name"]}</h3>
+        <h3>${song["file-name"]}</h3>
 
-//         <audio controls>
-//             <source src="${song["music_uri"]}">
-//         </audio>
+        <audio controls>
+            <source src="${song["music_uri"]}">
+        </audio>
 
-//         <div id="price">
-//             <p id="buy-price">buying price: <span>${song["buy-price"]}</span></p>
-//             <p id="sell-price">selling price: <span>${song["sell-price"]}</span></p>
-//         </div>
+        <div id="price">
+            <p id="buy-price">buying price: <span>${song["buy-price"]}</span></p>
+            <p id="sell-price">selling price: <span>${song["sell-price"]}</span></p>
+        </div>
 
-//         <div class="price-button">
-//             <button id="buy">buy</button>
-//             <button id="sell">sell</button>
-//         </div>
+        <div class="price-button">
+            <button id="buy">buy</button>
+            <button id="sell">sell</button>
+        </div>
 
-//         <button id="edit">Edit</button>
-//     </div>
-//     `
-// }
+        <button id="edit">Edit</button>
+    </div>
+    `
+}
